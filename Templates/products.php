09576257e221b09ff1c524f960b2e114ee721681
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-11-14 14:05:55 +0100

    updated products page

--- Templates/products.php
+++ Templates/products.php
@@ -7,44 +7,47 @@ include_once('defaults/head.php');
 
 <body>
 
-<div class="container">
+<div class="container bg-dark">
     <?php
     //adds the rest of the default files.
     include_once('defaults/header.php');
     include_once('defaults/menu.php');
-    include_once('defaults/pictures.php');
+    //    include_once('defaults/pictures.php');
     ?>
 
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/home">Home</a></li>
-            <li class="breadcrumb-item"><a href="/categories">Categories</a></li>
-        </ol>
-    </nav>
-    <div class="row gy-3 ">
-        <!--
-            this will loop through all the categories as category.
-            because category is an object you use "->" to get the specific data.
-            so if you want to see the id from a category you type $category->id
-        -->
-        <?php global $products ?>
-        <?php foreach ($products as $product): ?>
-            <div class="col-sm-4 col-md-3">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <a href="/category/<?= $product->id ?>">
-                            <img class="product-img img-responsive center-block" src='/img/<?= $product->picture ?>'/>
-                        </a>
-                        <div class="card-title mb-3"><?= $product->name ?></div>
+    <div class="bg-black text-light text-center p-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/home">Home</a></li>
+                <li class="breadcrumb-item"><a href="/categories">Categories</a></li>
+                <li class="breadcrumb-item"><a href="/category/<?= $params[2]; ?>">Products</a></li>
+            </ol>
+        </nav>
+        <div class="row gy-3">
+            <!--
+                this will loop through all the categories as category.
+                because category is an object you use "->" to get the specific data.
+                so if you want to see the id from a category you type $category->id
+            -->
+            <?php global $products; ?>
+            <?php foreach ($products as $product): ?>
+                <div class="col-sm-4 col-md-3 card-group">
+                    <div class='card bg-dark mx-2 border border-3 border-dark rounded-2' style='width: 18rem;'>
+                        <img src='/img/<?= $product->picture; ?>' class='card-img-top' alt='Image of $pageType'>
+                        <div class='card-body'>
+                            <h5 class='card-title text-white'><?= $product->name; ?></h5>
+                            <hr>
+                            <p class="card-text text-light"><?= $product->description; ?></p>
+                            <a href='/product/<?= $product->id; ?>' class='stretched-link'></a>
+                        </div>
                     </div>
-
                 </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
 
+        </div>
+
+        <hr>
     </div>
-
-    <hr>
     <?php
     include_once('defaults/footer.php');
 
