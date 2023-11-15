e289f52bec7c4cc6aaabf887e157535e108fdf84
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-11-15 11:59:18 +0100

    working on function for the category name

--- Modules/categories.php
+++ Modules/categories.php
@@ -18,5 +18,23 @@ function getCategories():array
 
 function getCategoryName(int $id):string
 {
+    global $pdo;
+    $categories = null;
 
+    switch ($params[1]) {
+        case "category":
+            //Grabs the id of the category from the link and searches the category name
+            $categories = $pdo->query('SELECT name FROM category WHERE id=$params[2]')->fetch(PDO::FETCH_CLASS, 'Category');
+            break;
+
+        case "product":
+            //Grabs the id of the product from the link and searches the product. Then display the category_id to
+            $categoryId = $pdo->query('SELECT category_id FROM product WHERE id=$params[2]')->fetch(PDO::FETCH_CLASS, 'Category');
+            $categoryId = $categoryId->category_id;
+            //Finds the category name from the fetched category id
+            $categories = $pdo->query('SELECT name FROM category WHERE id=$categoryId')->fetch(PDO::FETCH_CLASS, 'Category');
+            break;
+    }
+
+    return $categories;
 }
