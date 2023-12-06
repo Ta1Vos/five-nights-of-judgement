38be09f2b948aeee23a669d26f9e20a316305270
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-12-06 14:39:21 +0100

    improved login & register

--- Templates/login.php
+++ Templates/login.php
@@ -25,45 +25,45 @@ include_once('defaults/head.php');
         <div class="row gy-3">
             <form method="post" class="row text-light text-center">
                 <div class="col-12">
+                    <div>* First name</div>
+                    <small class="error-field">
+                        <?= $firstNameError; ?>
+                    </small>
+                    <br>
                     <label>
-                        <div>* First name</div>
-                        <small class="error-field">
-                            <?= $firstNameError; ?>
-                        </small>
-                        <br>
                         <input type="text" name="login-fname" value="<?= $firstName; ?>">
                     </label>
                 </div>
                 <br><br><br><br>
                 <div class="col-12">
+                    <div>* Last name</div>
+                    <small class="error-field">
+                        <?= $lastNameError; ?>
+                    </small>
+                    <br>
                     <label>
-                        <div>* Last name</div>
-                        <small class="error-field">
-                            <?= $lastNameError; ?>
-                        </small>
-                        <br>
                         <input type="text" name="login-lname" value="<?= $lastName; ?>">
                     </label>
                 </div>
                 <br><br><br><br>
                 <div class="col-12">
+                    <div>Email</div>
+                    <small class="error-field">
+                        <?= $emailError; ?>
+                    </small>
+                    <br>
                     <label>
-                        <div>Email</div>
-                        <small class="error-field">
-                            <?= $emailError; ?>
-                        </small>
-                        <br>
                         <input type="text" name="login-email" value="<?= $email; ?>">
                     </label>
                 </div>
                 <br><br><br><br>
                 <div class="col-12">
+                    <div>* Password</div>
+                    <small class="error-field">
+                        <?= $passwordError; ?>
+                    </small>
+                    <br>
                     <label>
-                        <div>* Password</div>
-                        <small class="error-field">
-                            <?= $passwordError; ?>
-                        </small>
-                        <br>
                         <input type="text" name="login-password" value="<?= $password; ?>">
                     </label>
                 </div>
