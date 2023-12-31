05678d2ec441262fc8dba1cfdf400e3419858fb3
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-12-31 16:02:40 +0100

    list member page

---
+++ Templates/admin/list-member.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<?php
+// Adds the head for the page.
+include_once('../Templates/defaults/head.php');
+?>
+
+<body>
+
+<div class="container bg-dark">
+    <?php
+    //adds the rest of the default files.
+    include_once('../Templates/defaults/header.php');
+    include_once('defaults/menu.php');
+    //    include_once('defaults/pictures.php');
+    ?>
+
+    <div class="bg-black text-light text-center p-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="home">Home</a></li>
+                <li class="breadcrumb-item"><a href="login">Log In</a></li>
+            </ol>
+        </nav>
+        <div class="row gy-3">
+            <form method="post" class="row text-light text-center">
+                <div class="col-12">
+                    <div>* Moderation action</div>
+                    <small class="error-field">
+                        <?= $moderationActionError; ?>
+                    </small>
+                    <br>
+                    <label>
+                        <input type="text" name="moderation-action" value="<?= $moderationActionInput; ?>">
+                    </label>
+                </div>
+                <br><br><br><br>
+                <div class="col-12">
+                    <div>Email</div>
+                    <div class="email-field">
+                        <?= $userEmail; ?>
+                    </div>
+                </div>
+                <br><br><br><br>
+                <div class="col-12">
+                    <div>* Delete user</div>
+                    <small class="error-field">
+                        <?= $deleteUserError; ?>
+                    </small>
+                    <div class="delete-user-field">
+                        <button><?= $deleteUser; ?></button>
+                    </div>
+                </div>
+                <br>
+            </form>
+            <div class="error-field">
+                <?= $mainErrorField; ?>
+            </div>
+        </div>
+
+        <hr>
+    </div>
+    <?php
+    include_once('defaults/footer.php');
+
+    ?>
+</div>
+
+</body>
+
+</html>
